b2bcbc9d5573c99ab975f58b049b3c640ba80f0d
Matthew R <matthew@mjrouser.com>
2015-03-30 14:33:43 -0400

    first scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dataPortalApp
+ * @description
+ * # dataPortalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dataPortalApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
