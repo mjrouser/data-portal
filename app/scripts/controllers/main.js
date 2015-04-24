ea3543ebac27343feec0b0deeb4a809b0153d42f
Matthew R <matthew@mjrouser.com>
2015-04-24 11:01:10 -0400

    responsive elements

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,61 +13,11 @@ angular.module('dataPortalApp')
 
      //Scrolls page to the selected section
      $scope.scrollTo = function(id) {
-          //var old = $location.hash();
+          
           $location.hash(id);
           $anchorScroll();
-          //$location.hash(old);
 
-     };
-
-
-/*
-
-
-  	//Functions for in-page nav
-     $scope.gotoLearn = function(){
-     	$location.hash('learn');
-
-     	$anchorScroll();
-       
-                 
-     };
-     $scope.gotoExplore = function(){
-     	$location.hash('explore');
-
-     	$anchorScroll();
-
-                   
-
-     };
-     $scope.gotoBuild = function(){
-     	$location.hash('build');
-
-     	$anchorScroll();
-
-                   
-
-     };
-     */
- /*
-    
-     //sticky for orange sub-nav
-     angular.element(document).ready(function(){
-     	angular.element('#sticky').affix({
-     		offset:{
-     			top: 475
-     		}
-     	});
-     });
-
-
-
-          angular.element('#sticky').affix({
-               offset:{
-                    top: 475
-               }
-          });
-  */       
+     };     
           
      //keeps column elements at same hight to align buttons
      
